9a2c5edf07c48eb5c984c7040dede907c8728ac5
ponak <ponakilan25@gmail.com>
2024-01-13 23:46:07 +0530

    Accuracy metric updated

--- helpers/accuracy_score.py
+++ helpers/accuracy_score.py
@@ -8,7 +8,7 @@ def accuracy_score(anchor_embed, pos_embed, neg_embed, threshold=0.3):
         pos_dist = abs(np.linalg.norm(anchor_embed[i]) - np.linalg.norm(pos_embed[i]))
         neg_dist = abs(np.linalg.norm(anchor_embed[i]) - np.linalg.norm(neg_embed[i]))
 
-        if pos_dist <= threshold < neg_dist:
+        if pos_dist < neg_dist:
             prediction.append(1)
         else:
             prediction.append(0)
